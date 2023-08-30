c2d8e89f50f4565f12df5e0f0df892d0a8652cc5
Parsa Harooni <prsishere@gmail.com>
2023-08-30 16:31:58 +0330

    Authentication with token works

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from core import views
 
 urlpatterns = [
-    
+    path('ping/', views.PingView.as_view(), name='ping'),
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class PingView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Pong'}
+        return Response(content)
