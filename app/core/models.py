42df60abbc75ad3929a0c0131decd08225382e1c
Parsa Harooni <prsishere@gmail.com>
2023-09-09 23:14:14 +0330

    admin model fix

--- app/core/models.py
+++ app/core/models.py
@@ -17,8 +17,14 @@ class Trip(models.Model):
     owner = models.ForeignKey(verbose_name='owner', to=User, on_delete=models.CASCADE)
     people = models.IntegerField(verbose_name='people')
     
+    def __str__(self) -> str:
+        return f"{self.country} - {self.city} - {self.people}"
+    
 class Expense(models.Model):
     reason = models.CharField(verbose_name='reason', max_length=128)
     price = models.IntegerField(verbose_name='price')
     category = models.CharField(verbose_name='category', choices=EXPENSE_CATEGORY, max_length=2)
     trip = models.ForeignKey(verbose_name='trip', to=Trip, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.reason} - {self.reason}"
